ec8dc4f61f91a4472222f174696002eabab8fdcf
subarkahisman <ismansubarkah.2210@gmail.com>
2025-07-31 14:08:23 +0700

    Design Login Page

--- App.tsx
+++ App.tsx
@@ -1,20 +1,65 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import {
+  Platform,
+  SafeAreaView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.containerForm}>
+        <Text style={styles.textXL}>Sign In</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Masukan Email"
+          keyboardType="email-address"
+          autoCapitalize="none"
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Masukan Password"
+          secureTextEntry
+          autoCapitalize="none"
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>Sign In</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    padding: Platform.OS === "android" ? StatusBar.currentHeight : 0,
+  },
+  containerForm: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  textXL: {
+    fontSize: 25,
+    fontWeight: "bold",
+    marginVertical: 15,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 15,
+    width: "100%",
+    marginBottom: 10,
+  },
+  button: {
+    width: "100%",
+    borderRadius: 15,
+    alignItems: "center",
+    backgroundColor: "orange",
+    padding: 10,
   },
 });
